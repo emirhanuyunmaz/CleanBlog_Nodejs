a19dc62ef5b2abf7cfa048ddb3bfbcad1dafcdc6
Emirhan Uyunmaz <emirhanuyunmaz46@gmail.com>
2023-12-07 22:48:05 +0300

    ejs added

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require("express")
-
 const app = express()
+const path = require("path")
+const ejs = require("ejs")
+
+//Burada ejs ile .html dosyalarını .ejs çevirerek kullanma işlemi yapmaktayız.
+app.set("view engine" ,"ejs")
 
 const blog = { id: 1, title: "Blog title", description: "Blog description" }
 
+//Burada public dosyasında bulunan verileri kullanma işlemini yapmamızı sağlar. 
+app.use(express.static("public"))
+
 app.get("/",(req,res) => {
-    res.send(blog)
+    res.render("index")
 })
+
+app.get("/about",(req,res) => {
+    res.render("about")
+})
+
+app.get("/addpost",(req,res) => {
+    res.render("add_post")
+})
+
 
 const port = 3000
 
